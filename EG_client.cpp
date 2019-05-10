31dd07aa1eb7a5c643fc45bfbd22a6f029fe4f71
Alexander Levsha <levsha.alexander@gmail.com>
2019-05-10 19:40:44 +0300

    added the self-install mechanism
    self-install ourselves into Libraries section of dacom.ini
    dacom.ini is kept unmodded so that users can install their own binary addons
    
    however, attaching the DLL in freelancer.ini's Resources section is too late
    some objects (archetypes?) are already loaded at that point
    and we cannot modify their rendering ranges
    
    by self-installing ourselves into dacom.ini we fix this issue for all consequent game launches

--- EG_client.cpp
+++ EG_client.cpp
@@ -1,3 +1,4 @@
+#include <IO.H>
 #include "EG_client.h"
 #include "Preferences.h"
 
@@ -16,6 +17,53 @@ HMODULE	hModSelf, hModFL;
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
+void DacomSelfInstall()
+{
+	wchar_t wszModPath[MAX_PATH];
+	GetModuleFileName(hModSelf, wszModPath, MAX_PATH);
+
+	std::wstring wscModPath(wszModPath);
+	size_t posSep = wscModPath.find_last_of(L'\\');
+
+	std::wstring wscIniPath = wscModPath.substr(0, posSep + 1) + L"dacom.ini";
+	std::string scMod = wstos(wscModPath.substr(posSep + 1).c_str(), CP_ACP);
+
+	std::ci_string scIni;
+	FILE *fp = _wfopen(wscIniPath.c_str(), L"r+b");
+	try
+	{
+		if (!fp)
+			throw L"_wfopen";
+		if (fseek(fp, 0, SEEK_END))
+			throw L"fseek ";
+		long fileSize = ftell(fp);
+		if (EOF == fileSize)
+			throw L"ftell";
+		scIni.resize(fileSize);
+		rewind(fp);
+		if (fread(&scIni[0], 1, fileSize, fp) != fileSize)
+			throw L"fread";
+
+		size_t posSection = scIni.find("[libraries]");
+		size_t posNext = scIni.find('[', posSection + 1);
+		size_t posDll = scIni.find(scMod.c_str(), posSection + 12);
+		if (std::string::npos == posDll || posDll > posNext)
+		{
+			scIni.insert(posNext, "\r\n");
+			scIni.insert(posNext, scMod.c_str());
+			rewind(fp);
+			if (_chsize(_fileno(fp), 0))
+				throw L"_chsize";
+			if (fwrite(scIni.c_str(), 1, scIni.length(), fp) != scIni.length())
+				throw L"fwrite";
+		}
+	}
+	CATCH(L"DacomSelfInstall", DO_NOTHING)
+
+	if (fp)
+		fclose(fp);
+}
+
 BOOL CALLBACK DllMain(HINSTANCE hModule, DWORD fdwReason, LPVOID lpReserved)
 {
 	if(!lpReserved)
@@ -33,6 +81,8 @@ BOOL CALLBACK DllMain(HINSTANCE hModule, DWORD fdwReason, LPVOID lpReserved)
 			hModSelf = hModule;
 			hModFL = GetModuleHandle(NULL);
 
+			DacomSelfInstall();
+			
 			if(!InitPrefs())
 				return FALSE;
 
